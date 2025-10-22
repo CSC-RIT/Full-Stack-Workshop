5ad7e01ddd9796f584183d00226ff717c375e221
Inferno-idk-what-to-do <matthewfiedler19@gmail.com>
2025-10-22 12:32:28 -0400

    backend starter code

---
+++ backend/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import json
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+TASKS_FILE = 'tasks.json'
+
+# read whole file to python list
+def read_tasks_file():
+    with open(TASKS_FILE, 'r') as file:
+        return json.load(file)
+
+# TODO: completely overwrite file with <tasks>
+def write_to_file(tasks):
+    pass
+
+# run with `curl 127.0.0.1:5000/tasks`
+@app.route('/tasks', methods=['GET'])
+def get_all_tasks():
+    return jsonify(read_tasks_file())
+
+# TODO: run with `curl -X PUT 127.0.0.1:5000/tasks/TASK_NUMBER -H "Content-Type: application/json" -d '{"status": "NEW_STATUS"}'`
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task_status(task_id):
+    return jsonify({'error': 'unimplemented!'}), 501 
